895b3186be8120bd61de9ec98a4178d14ebfe42a
Rafael Rossi <rtrafaeltrova@gmail.com>
2023-05-05 01:17:36 -0300

    Criando o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-import {  } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+
+dotenv.config(); // Carrega o arquivo ".env" com suas configuracoes
+
+const server = express();
+
+server.set("view engine", "mustache"); // Seto o view engine a ser utilizado no projeto
+server.set("views", path.join(__dirname, "views")); // Seto uma pasta para armazenar as views do projeto
+server.engine("mustache", mustache()); // Passo o callback da view engine a ser utilizada
+
+server.use(express.static(path.join(__dirname, "../public"))); // Seto a pasta estatica(publica) que vai conter os arquivos css e imagens
+
+// Rotas
+
+server.listen(process.env.PORT);
