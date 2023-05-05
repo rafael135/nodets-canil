495d32721231d5e91e3c62624f4e76db07da18d9
Rafael Rossi <rtrafaeltrova@gmail.com>
2023-05-05 16:39:18 -0300

    Criando as rotas e controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config(); // Carrega o arquivo ".env" com suas configuracoes
 
@@ -14,5 +16,10 @@ server.engine("mustache", mustache()); // Passo o callback da view engine a ser 
 server.use(express.static(path.join(__dirname, "../public"))); // Seto a pasta estatica(publica) que vai conter os arquivos css e imagens
 
 // Rotas
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+    res.send("Pagina nao encontrada");
+});
 
 server.listen(process.env.PORT);
