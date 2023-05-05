7e2c1932d0080fa72ad648746f2e00d61c1fdef9
Rafael Rossi <rtrafaeltrova@gmail.com>
2023-05-05 16:52:15 -0300

    Separando as views

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send("home");
-    //res.render("pages/page");
+    //res.send("home");
+    res.render("pages/page");
 };
 
 export const dogs = (req: Request, res: Response) => {
-    res.send("dogs");
-    //res.render("pages/page");
+    //res.send("dogs");
+    res.render("pages/page");
 };
 
 export const cats = (req: Request, res: Response) => {
-    res.send("cats");
-    //res.render("pages/page");
+    //res.send("cats");
+    res.render("pages/page");
 };
 
 export const fishes = (req: Request, res: Response) => {
-    res.send("fishes");
-    //res.render("pages/page");
+    //res.send("fishes");
+    res.render("pages/page");
 };
